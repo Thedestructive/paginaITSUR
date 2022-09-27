481a9ef6d32954d3bed2ef21f08054b692644f62
hugogaytan10 <estardus9@gmail.com>
2022-09-27 09:17:44 -0500

    rutas

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import img from '../../isc.jpg'
 import './News.css'
 export const News = () => {
+    const refNew = createRef();
     const contenent_news = [
         {
             image: img,
@@ -16,14 +17,23 @@ export const News = () => {
             new: 2
         }
     ]
+    const WatchNew = async(index) => {
+        refNew.current.style.visibility = 'visible';
+    } 
+    const HiddenPopUp = async() => {
+        refNew.current.style.visibility = 'hidden';
+    }
     return (
         <section className='noticias'>
             <h1 className='titulo'>Noticias</h1>
+            <div className='popUpNews' ref={refNew}>
+                <div onClick={()=>{HiddenPopUp()}} className='close-popup'><p>X</p></div>
+            </div>
             {
                 contenent_news.map((new_item, idx) => { return (
-                    <article className='articulo'>
+                    <article className='articulo' key={idx}>
                         <img src={new_item.image}/>
-                        <button className='btn-noticias'>Ver mas...</button>
+                        <button className='btn-noticias' onClick={()=>{WatchNew(idx)}}>Ver mas...</button>
                     </article>
                 )})
             }
