f604728b8ccc7647e3252efa45a26dab8b981cb7
hugogaytan10 <estardus9@gmail.com>
2022-10-28 21:49:28 -0500

    header de inicio

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,39 +1,44 @@
-import React, { createRef } from 'react'
-import img from '../../isc.jpg'
+import React, { createRef, useState } from 'react'
 import imgNoticia from './img_Noticia.jpg'
 import imgNoticia2 from './img_Noticia2.png'
 import imgNoticia3 from './img_Noticia3.png'
 import './News.css'
+import { FaEye } from 'react-icons/fa';
 
 export const News = () => {
     const refNew = createRef();
     const refImage = createRef();
     const refText = createRef();
+    const [link, setLink] = useState('');
     let positionUser;
     const contenent_news = [
         {
             image: imgNoticia,
             new: 0,
             textMuestra: "CAPACÍTATE Y CERTIFÍCATE",
-            textPopUp: "CAPACÍTATE Y CERTIFÍCATE INTERNACIONALMENTE EN LAS TECNOLOGÍAS DE INGENIERÍA WWW.XPERTCAD.COM"
+            textPopUp: "CAPACÍTATE Y CERTIFÍCATE INTERNACIONALMENTE EN LAS TECNOLOGÍAS DE INGENIERÍA WWW.XPERTCAD.COM",
+            link: ''
         },
         {
             image: imgNoticia2,
             new: 1,
             textMuestra: " CONVOCATORIA RED ECOS BECAS STUDY BUENOS AIRES",
-            textPopUp:  "http://www.itsur.edu.mx/documentos_publicados/investigacion/2022/Fact_sheet_StudyBuenosAires_2022.pdf"
+            textPopUp: '',
+            link:  "http://www.itsur.edu.mx/documentos_publicados/investigacion/2022/Fact_sheet_StudyBuenosAires_2022.pdf"
         },
         {
             image: imgNoticia3,
             new: 2,
             textMuestra: "CONVOCATORIA PARA EL 2DO. CONGRESO INTERNACIONAL DE INNOVACIÓN EN INGENIERÍA INDUSTRIAL, GESTIÓN Y COMPUTACIÓN (INGECO)",
-            textPopUp: "http://www.itsur.edu.mx/documentos_publicados/investigacion/2022/Convocatoria_INGECO.pdf"
+            textPopUp: '',
+            link: "http://www.itsur.edu.mx/documentos_publicados/investigacion/2022/Convocatoria_INGECO.pdf"
         }
     ]
-    const WatchNew = async (img, text, idNew) => {
+    const WatchNew = async (img, text, idNew, link) => {
         refNew.current.style.visibility = 'visible';
         refImage.current.src = img;
         refText.current.innerHTML = text;
+        setLink(link);
         positionUser = idNew;
         window.scroll(0,0);
         await disableScroll();
@@ -58,16 +63,25 @@ export const News = () => {
             <div className='popUpNews' ref={refNew}>
                 <div onClick={() => { HiddenPopUp() }} className='close-popup'><p>X</p></div>
                 <div className="papaPopUp"> <img className="imagenPopUp" ref={refImage} /> </div>
-                <p className="textoPopUp" ref={refText}> </p>
+                <p ref={refText} className='textoPopUp'></p>
+                {
+                    link !== '' &&
+                    <div className='contenedor-link'>
+                        <FaEye className='icon-ojo'/>
+                        <a  href={link} className='btn-irA'> Ver documento</a>
+                    </div>
+                }
             </div>
             <div className='contenedor-noticias'>
                 {
                     contenent_news.map((new_item, idx) => {
                         return (
                             <article className='card' key={idx} id = {idx}>
-                                <img src={new_item.image} />
-                                <p className = "textMuestraNoticia"> {new_item.textMuestra} </p>
-                                <button className='btn-noticias' onClick={() => { WatchNew(new_item.image, new_item.textPopUp, idx) }}>Ver mas...</button>
+                                <div className='contenido-card'>
+                                    <img src={new_item.image} />
+                                    <p className = "textMuestraNoticia"> {new_item.textMuestra} </p>
+                                    <button className='btn-noticias' onClick={() => { WatchNew(new_item.image, new_item.textPopUp, idx, new_item.link) }}>Ver mas...</button>
+                                </div>
                             </article>
                         )
                     })
