3580c2e13836c91346c91a92ba3c77d42400124e
hugogaytan10 <estardus9@gmail.com>
2022-11-21 20:21:36 -0600

    termino de plan de estudio

--- src/components/PlanEstudio/Vision.jsx
+++ src/components/PlanEstudio/Vision.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { FaArrowRight } from 'react-icons/fa'
 export const Vision = ({ vision }) => {
     return (
         vision.map((item, idx) => {
             return (
-                <p key={idx} className='item-plan'>{item}</p>
+                <p key={idx} className='item-plan'><FaArrowRight className='flecha-plan-estudio'/> {item}</p>
             )
         })
     )
